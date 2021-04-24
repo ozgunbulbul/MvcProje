f79fef0ed07761a5372aa6c781a6e60e5026fcf8
ozgunbulbul <58927634+ozgunbulbul@users.noreply.github.com>
2021-04-24 15:05:16 +0300

    Initial commit

---
+++ DataAccesLayer/Concreate/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concreate
+{
+    public class Context : DbContext
+    {
+        //sql e tablo issimlei olarak yansıtacak.
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
